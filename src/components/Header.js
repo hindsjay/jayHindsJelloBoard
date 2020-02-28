335143d9207b98bd16b6779fd40475b1826b8714
Jay Hinds <hinds.jason@gmail.com>
2020-02-28 09:25:09 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Title</h1>
+      <div class="sub-header">
+        <p>Jay Hinds</p>
+        <button>Menu</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
