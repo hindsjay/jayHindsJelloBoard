6783184616658bada04b43755fcc84016f2c5d8e
Jay Hinds <hinds.jason@gmail.com>
2020-02-29 23:49:49 -0500

    add code for adding input data to firebase database

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 
-const Form = () => {
+const Form = (props) => {
   return (
     <form>
       <label htmlFor="task-input" className="sr-only"></label>
-      <input type="text" id="task-input" className="task-input" placeholder="Input Task Here..."></input>
-      <button type="submit">Add Task</button>
+      <input 
+        type="text" 
+        id="task-input" 
+        className="task-input" 
+        placeholder="Input Task Here..."
+        onChange={ (event) => {props.inputVal(event)} }
+        value={props.userInputState}
+      >
+      </input>
+      <button type="submit" onClick={ (event) => {props.handleClick(event)} }>Add Task</button>
     </form>
   )
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,67 @@
 import React, { Component } from 'react';
 import Form from './Form.js';
+import firebase from '../utils/firebase.js';
+
 
 class Main extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      tasks: [],
+      userInput: '',
+    }
+  }
+
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+
+    // event listener that fires anytime there is a change in the database
+    dbRef.on('value', (response) => {
+      const newState = [];
+      const data = response.val();
+      console.log(data);
+
+      // for in loop to access each task item in our data object and push each task item to our new state array
+      for (let key in data) {
+        const dataObject = {};
+
+        dataObject.id = key;
+        dataObject.value = data[key];
+
+        newState.push(dataObject);
+      }
+
+      // execute setState to initiate re-render which will update our page with the task items that have been added to state
+      this.setState({
+        tasks: newState,
+      })
+    });
+  }
+
+
+  handleChange = (event) => {
+    this.setState({
+      userInput: event.target.value,
+    })
+  }
+
+
+  handleClick = (event) => {
+    // prevent default action on form submission
+    event.preventDefault();
+    // reference to database
+    const dbRef = firebase.database().ref();
+    // whatever value is in state for userInput on submission/button click, we want to add this to our database
+    dbRef.push(this.state.userInput);
+    // reset state of input so it's blank
+    this.setState({
+      userInput: '',
+    })
+  }
+
+
   render() {
     return(
       <main>
@@ -10,39 +70,33 @@ class Main extends Component {
             <section className="card-container">
               <h2>Task List</h2>
               <div className="task-item-container">
-                <div className="task-item">
-                  <p>This is task #1 - Test</p>
-                </div>
-                <div className="task-item"></div>
-                <div className="task-item"></div>
+              {
+                this.state.tasks.map((task) => {
+                  return (
+                    <div key={task.id} className="task-item">
+                      <p>{task.value}</p>
+                    </div>
+                  )
+                })
+              }
               </div>
             </section>
 
             <section className="card-container">
               <h2>In Progress</h2>
-              <div className="task-item-container">
-                <div className="task-item">
-                  <p>This is task #1 - Test</p>
-                </div>
-                <div className="task-item"></div>
-                <div className="task-item"></div>
-              </div>
             </section>
 
             <section className="card-container">
               <h2>Done</h2>
-              <div className="task-item-container">
-                <div className="task-item">
-                  <p>This is task #1 - Test</p>
-                </div>
-                <div className="task-item"></div>
-                <div className="task-item"></div>
-              </div>
             </section>
           </div>
         </div>
 
-        <Form />
+        <Form 
+          inputVal={this.handleChange} 
+          userInputState={this.state.userInput} 
+          handleClick={this.handleClick}
+        />
       </main>
     )
   }
