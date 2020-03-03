b9849454493e65f5f044fb49515643826794b523
Jay Hinds <hinds.jason@gmail.com>
2020-03-02 20:40:13 -0500

    updated state and imported Welcome component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
-// import '../App.css';
+import React, { Component, Fragment } from 'react';
+import Welcome from './Welcome.js';
 import Header from './Header.js';
 import Main from './Main.js';
 
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notEntered: true,
+    }
+  }
+
+
+  toggleEnteredState = () => {
+
+    this.setState({
+      notEntered: !this.state.notEntered
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
+        {
+          this.state.notEntered 
+            ? <Welcome enterButtonClicked={this.toggleEnteredState} /> 
+            : <Fragment>
+                <Header />
+                <Main />
+              </Fragment>
+        }
       </div>
     );
   }
