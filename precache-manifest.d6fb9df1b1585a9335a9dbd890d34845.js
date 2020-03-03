2ef953f358bf0051d22d92123f91bd442fc82562
Jay Hinds <hinds.jason@gmail.com>
2020-03-03 15:13:58 -0500

    Updates

---
+++ precache-manifest.d6fb9df1b1585a9335a9dbd890d34845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235e059958f2a480fc787cfec4d3bc36",
+    "url": "/jayHindsJelloBoard/index.html"
+  },
+  {
+    "revision": "7a929ff7422ca073ad06",
+    "url": "/jayHindsJelloBoard/static/css/main.6299036d.chunk.css"
+  },
+  {
+    "revision": "0d43c4e60fef13519d3c",
+    "url": "/jayHindsJelloBoard/static/js/2.6613fb7a.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/jayHindsJelloBoard/static/js/2.6613fb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a929ff7422ca073ad06",
+    "url": "/jayHindsJelloBoard/static/js/main.1a942bba.chunk.js"
+  },
+  {
+    "revision": "3a795be2c3d05c845eae",
+    "url": "/jayHindsJelloBoard/static/js/runtime-main.67e359a7.js"
+  }
+]);
