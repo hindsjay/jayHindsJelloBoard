c54d81725a6cec8843934b6ea8a5ca8c91866488
Jay Hinds <hinds.jason@gmail.com>
2020-03-03 19:05:36 -0500

    updated title name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,9 +5,9 @@ const Welcome = (props) => {
   return (
     <header className="welcomeHeaderContainer">
       <div className="welcomeContentContainer">
-        <h1 className="welcomeHeader">Title</h1>
+        <h1 className="welcomeHeader">Jello</h1>
 
-        <p>Welcome to Title!</p>
+        <p>Welcome to Jello!</p>
         <p>A Trello like task board to keep you organized and focused on the most important tasks</p>
 
         <button type="button" onClick={ () => {props.enterButtonClicked()} }>Enter</button>
