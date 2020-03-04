3e8cf292041c61dc9524f23c7c660bbd7236daa3
Jay Hinds <hinds.jason@gmail.com>
2020-03-03 19:08:05 -0500

    update title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 // stateless component
-// *** special note - the menu button isn't functional at this time.  Included it because I wanted to include some functionality but this was a stretch goal and I just concentrated on getting the MVP completed.
 const Header = () => {
   return (
     <header className="mainAppHeader">
-      <h1>Title</h1>
-      <div className="subHeader wrapper">
-        <p>Jay Hinds</p>
-        <button>Menu</button>
-      </div>
+      <h1>Jello</h1>
     </header>
   )
 }
