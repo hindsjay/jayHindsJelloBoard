de00a0c1f757819b7d7d889180181e3cd0002b4d
Jay Hinds <hinds.jason@gmail.com>
2020-04-11 20:16:03 -0400

    updated to class component, added state, added methods from Main component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,85 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import Edit from './Edit.js';
 
-// stateless component
-const Task = (props) => {
-  return (
-    <Fragment>
-      <p>{props.taskValue}</p> 
-      <div className="editDeleteContainer">
-        <button type="button" onClick={props.editTask}>edit</button>
-        <button type="button" onClick={props.removeTask}>delete</button>
-      </div>
-    </Fragment>
-  )
+
+class Task extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      editing: false,
+      editingInputValue: '',
+    };
+  }
+
+  
+  // fires each time a user inputs a value into the input field when editing a task
+  editHandleChange = (event) => {
+    this.setState({
+      editingInputValue: event.target.value,
+    })
+  }
+
+
+  // function to remove item
+  removeTask = (referenceToDb, task) => {
+    referenceToDb.ref().child(task).remove();
+  }
+
+
+  // function to edit task item - fires when edit button is clicked
+  editTask = (taskValue) => {
+
+    this.setState({
+      editing: !this.state.editing,
+      editingInputValue: taskValue,
+    })
+  }
+
+
+  // save task function - fires when save button is clicked in "editing" mode
+  saveTask = (referenceToDb, task) => {
+    // reference to our database but reference to the specific task item we want to update the value of
+    const taskItemInDb = referenceToDb.ref(task);
+    // updating the task value in our database to the value that's in our editingInput state
+    taskItemInDb.set(this.state.editingInputValue);
+
+    // update state and initiate re-render
+    this.setState({
+      editing: !this.state.editing,
+      editingInputValue: '',
+    })
+  } 
+
+
+  render() {
+    return (
+      <Fragment>
+        {
+         this.state.editing ? 
+           <Edit
+             editInputValue={this.state.editingInputValue}
+             editHandleChange={this.editHandleChange}
+             saveTask={ () => {this.saveTask(this.props.dbRefInfo, this.props.taskKey)} }
+           /> :
+           <div>
+              <p>{this.props.taskValue}</p> 
+              <div className="editDeleteContainer">
+                <button 
+                  type="button" 
+                  onClick={ () => {this.editTask(this.props.taskValue)} }>edit</button>
+                
+                <button 
+                  type="button" 
+                  onClick={ () => {this.removeTask(this.props.dbRefInfo, this.props.taskKey)} }
+                  >delete
+                </button>
+              </div>
+            </div>
+        }
+      </Fragment>
+    )
+  }
 }
 
 export default Task;
