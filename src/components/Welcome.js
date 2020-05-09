4f155f0830ea4d5d2708469d37a8a9a3365157a4
Jay Hinds <hinds.jason@gmail.com>
2020-05-09 09:27:59 -0400

    updated styles for image on welcome page and added in imported svg as component type

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as HeaderSvg } from '../assets/header-image.svg'
 
 
 // stateless component
@@ -6,13 +7,18 @@ import React from 'react';
 function Welcome({ enterButtonClicked }) {
   return (
     <header className="welcomeHeaderContainer">
-      <div className="welcomeContentContainer">
-        <h1 className="welcomeHeader">Jello</h1>
+      <h1 className="welcomeHeader">Jello</h1>
 
-        <p>Welcome to Jello!</p>
-        <p>A Trello like task board!  Create, view, update, and delete items to keep you organized and focused on the most important tasks</p>
+      <div className="welcomePageFlexContainer">
+        <div className="welcomePageImageContainer">
+          <HeaderSvg className="welcomePageImage" />
+        </div>
 
-        <button type="button" onClick={ () => {enterButtonClicked()} }>Get Tasking!</button>
+        <div className="welcomeContentContainer">
+          <p>Welcome to Jello!</p>
+          <p>A Trello like task board!  Create, view, update, and delete items to keep you organized and focused on the most important tasks</p>
+          <button type="button" onClick={ () => {enterButtonClicked()} }>Get Tasking!</button>
+        </div>
       </div>
     </header>
   )
